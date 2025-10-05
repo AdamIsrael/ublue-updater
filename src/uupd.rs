3047637410429d3d3ae27fd5e5d3f025f79fca14
Adam Israel <adam@adamisrael.com>
2025-10-04 22:04:54 -0400

    Refactor: use relm crate
    Refactor the application to use the `relm4` crate, which is an
    elm-inspired framework on top of gtk4-rs. One of the reasons I decided
    to look at this was because it makes threading in a gtk4 app easier.

---
+++ src/uupd.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Deserialize, Serialize, Debug)]
+pub struct Progress {
+    pub level: String,
+    pub msg: String,
+
+    #[serde(default)]
+    pub title: String,
+
+    #[serde(default)]
+    pub description: String,
+
+    #[serde(default)]
+    pub progress: u32,
+
+    #[serde(default)]
+    pub total: u32,
+
+    #[serde(default)]
+    pub step_progress: u32,
+
+    #[serde(default)]
+    pub overall: u32,
+}
